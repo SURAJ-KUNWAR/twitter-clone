cf32bfecea01d1322772629b36adb731fe4a14a8
Suraj <kunwarsuraj19@gmail.com>
2023-08-31 19:53:50 +0530

    grid typo

--- app.vue
+++ app.vue
@@ -1,12 +1,12 @@
 <template>
   <div :class="{'dark' : darkMode}">
- <div class="min-h-full">
-  <div class="gird grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+    <div clas="min-h-full">
+      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
 
     <!-- left part -->
     <div class="hidden  bg-red-500  md:block xs-col-span-1 xl:col-span-2" >
       <div class="sticky top-0">
-        fdfdfdsf
+        asd
       </div>
       
     </div>
