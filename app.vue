c3a375313f7e464b155e2181a523c4c77a041ef9
Suraj <kunwarsuraj19@gmail.com>
2023-08-31 19:39:55 +0530

    intial setup

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="{'dark' : darkMode}">
+ <div class="min-h-full">
+  <div class="gird grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+
+    <!-- left part -->
+    <div class="hidden  bg-red-500  md:block xs-col-span-1 xl:col-span-2" >
+      <div class="sticky top-0">
+        fdfdfdsf
+      </div>
+      
+    </div>
+
+    <!-- main content -->
+
+
+    <!-- right part -->
+
+  </div>
+
+ </div>
+  </div>
+</template>
+
+<script setup>
+const darkMode  =ref(true)
+</script>
